ea1cb79c9386f65c4c07bf43ed79c0e28249afab
yxDeveloper <dilutian0415@gmail.com>
2020-12-15 00:01:06 +0800

    myTest

---
+++ myTest/hellow.vue
@@ -0,0 +1,39 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-12-14 22:58:55
+ * @LastEditTime: 2020-12-14 23:23:50
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \my-test\hellow.vue
+-->
+<template>
+    <div>
+        <ul v-for="item in items" :key="item.id">
+            {{ item }}
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                key: value
+            }
+        },
+        methods: {
+            name() {
+                
+            }
+        },
+        computed: {
+            name() {
+                return this.data 
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
