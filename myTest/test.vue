1a22610a9b43c51679cd314b253bfc8dbc098c54
yxDeveloper <dilutian0415@gmail.com>
2020-12-26 23:51:32 +0800

    测试Vue插件

---
+++ myTest/test.vue
@@ -0,0 +1,23 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-12-26 23:50:14
+ * @LastEditTime: 2020-12-26 23:50:42
+ * @LastEditors: Please set LastEditors
+ * @Description: the test vue file
+ * @FilePath: \vue\myTest\test.vue
+-->
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
