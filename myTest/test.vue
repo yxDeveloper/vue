b6125e593a5632b83d3f97b405265d250c7afed4
yxDeveloper <dilutian0415@gmail.com>
2020-12-27 23:59:50 +0800

    组件中data当做方法使用，mouted周期（渲染结束）

---
+++ myTest/test.vue
@@ -0,0 +1,38 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-12-26 23:50:14
+ * @LastEditTime: 2020-12-27 23:59:12
+ * @LastEditors: Please set LastEditors
+ * @Description: the test vue file
+ * @FilePath: \vue\myTest\test.vue
+-->
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    import vue from "./vue.js"
+    new Vue({
+        data() {
+            
+        },
+        methods: {
+            
+        },
+        created: {
+            
+        },
+        mouted: {
+            
+        }
+    })
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
