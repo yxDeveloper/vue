b27ab691e010b5946618196d798bfcabc9e1bf17
yxDeveloper <dilutian0415@gmail.com>
2020-12-30 23:26:03 +0800

    todolist for Vue

---
+++ todolist.vue
@@ -0,0 +1,27 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-12-30 23:24:19
+ * @LastEditTime: 2020-12-30 23:25:12
+ * @LastEditors: Please set LastEditors
+ * @Description: todolist for Vue
+ * @FilePath: \vue\myTest\todolist.vue
+-->
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
