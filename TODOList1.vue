b77143aabdfb2e233fe8b1906f5fd2425e9a08dc
yxDeveloper <dilutian0415@gmail.com>
2021-01-10 20:50:54 +0800

    add vue

---
+++ TODOList1.vue
@@ -0,0 +1,24 @@
+Vue.directive('directiveName', {
+    bind(el, binding, vnode) {
+        el.style.arg = binding.value.arg;
+    }
+});
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
