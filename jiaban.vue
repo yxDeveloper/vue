062620f54b76b262d4e456df1a6e6bfae3a8e824
yxDeveloper <dilutian0415@gmail.com>
2021-01-13 23:42:27 +0800

    jiaban

---
+++ jiaban.vue
@@ -0,0 +1,23 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-01-13 23:41:54
+ * @LastEditTime: 2021-01-13 23:42:14
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue\jiaban.vue
+-->
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
