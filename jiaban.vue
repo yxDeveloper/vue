019a54189ce902c7b56a328b4b636d3c94407277
yxDeveloper <dilutian0415@gmail.com>
2021-01-14 23:55:26 +0800

    head

--- jiaban.vue
+++ jiaban.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-01-13 23:41:54
- * @LastEditTime: 2021-01-14 23:55:05
+ * @LastEditTime: 2021-01-14 23:55:21
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue\jiaban.vue
@@ -9,6 +9,7 @@
 <template>
     <div>
         <p></p>
+        <head></head>
     </div>
 </template>
 
