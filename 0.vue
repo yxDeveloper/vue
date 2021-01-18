22a8aee2f622907d0b662f44c14cfdb25ab99332
yxDeveloper <dilutian0415@gmail.com>
2021-01-18 21:58:03 +0800

    vue

--- 0.vue
+++ 0.vue
@@ -1,9 +1,23 @@
 <!--
  * @Author: your name
  * @Date: 2021-01-17 23:58:40
- * @LastEditTime: 2021-01-18 00:01:27
+ * @LastEditTime: 2021-01-18 21:57:46
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue\0.vue
 -->
-000
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
