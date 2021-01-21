15de5759847047c37946ac988666743f613871b6
yxDeveloper <dilutian0415@gmail.com>
2021-01-21 23:44:17 +0800

    vue-bace

--- vue.vue
+++ vue.vue
@@ -1,8 +1,27 @@
 <!--
  * @Author: your name
  * @Date: 2021-01-21 23:42:31
- * @LastEditTime: 2021-01-21 23:43:05
+ * @LastEditTime: 2021-01-21 23:44:06
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue\vue.vue
 -->
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
