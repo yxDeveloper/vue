7983c86990caede57e90a3ad177c2d070646bcc5
yxDeveloper <dilutian0415@gmail.com>
2021-01-24 00:01:06 +0800

    public

--- vue.vue
+++ vue.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-01-21 23:42:31
- * @LastEditTime: 2021-01-21 23:44:06
+ * @LastEditTime: 2021-01-24 00:00:58
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue\vue.vue
@@ -15,7 +15,7 @@
 <script>
 export default {
     setup () {
-        pu
+        public
 
         return {}
     }
