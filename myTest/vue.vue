b79b2db69c89f2dfc83293a28b745c08d9f918b3
yxDeveloper <dilutian0415@gmail.com>
2021-01-27 22:28:06 +0800

    template

---
+++ myTest/vue.vue
@@ -0,0 +1,23 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-01-27 22:27:52
+ * @LastEditTime: 2021-01-27 22:27:52
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue\myTest\vue.vue
+-->
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
