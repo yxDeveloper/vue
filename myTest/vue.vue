cb60afe8b181561e968588ccd7a3b6dd489baa9d
yxDeveloper <dilutian0415@gmail.com>
2021-01-27 22:28:36 +0800

    export_default

--- myTest/vue.vue
+++ myTest/vue.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-01-27 22:27:52
- * @LastEditTime: 2021-01-27 22:27:52
+ * @LastEditTime: 2021-01-27 22:28:19
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue\myTest\vue.vue
@@ -14,7 +14,7 @@
 
 <script>
     export default {
-        
+        // { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Montserrat:600|Rufina:700' }
     }
 </script>
 
