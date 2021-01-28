8c42ea2afebc17ad4bd7f014f0aa2ff90099f2af
yxDeveloper <dilutian0415@gmail.com>
2021-01-28 22:07:40 +0800

    float

--- myTest/vue.vue
+++ myTest/vue.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-01-27 22:27:52
- * @LastEditTime: 2021-01-28 22:06:49
+ * @LastEditTime: 2021-01-28 22:07:24
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue\myTest\vue.vue
@@ -24,5 +24,8 @@
 div {
     display: block;
     text-align: center;
+    header {
+        float: left;
+    }
 }
 </style>
