69576a353e1c65abd882b870c8b3a86bda85e58d
yxDeveloper <dilutian0415@gmail.com>
2021-01-28 22:05:31 +0800

    head

--- myTest/vue.vue
+++ myTest/vue.vue
@@ -1,14 +1,14 @@
 <!--
  * @Author: your name
  * @Date: 2021-01-27 22:27:52
- * @LastEditTime: 2021-01-28 22:05:01
+ * @LastEditTime: 2021-01-28 22:05:24
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue\myTest\vue.vue
 -->
 <template>
     <div>
-
+        <header></header>
     </div>
 </template>
 
