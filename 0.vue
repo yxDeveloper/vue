10dcd5f8447a7a9c21290e4d41d1800c079c9739
yxDeveloper <dilutian0415@gmail.com>
2021-02-05 22:25:54 +0800

    delete

--- 0.vue
+++ 0.vue
@@ -1,18 +1,16 @@
 <!--
  * @Author: your name
  * @Date: 2021-01-17 23:58:40
- * @LastEditTime: 2021-02-05 22:24:15
+ * @LastEditTime: 2021-02-05 22:25:42
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue\0.vue
 -->
 <template>
     <div>
-vue
+    vue
     </div>
-    <div>
-        
-    </div>
+
 </template>
 
 <script>
