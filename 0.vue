150707ea2a884af845830622cbfde704fa777221
yxDeveloper <dilutian0415@gmail.com>
2021-02-05 22:26:22 +0800

    block

--- 0.vue
+++ 0.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-01-17 23:58:40
- * @LastEditTime: 2021-02-05 22:25:42
+ * @LastEditTime: 2021-02-05 22:26:15
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue\0.vue
@@ -20,5 +20,7 @@
 </script>
 
 <style lang="scss" scoped>
-
+div {
+    display: block;
+}
 </style>
