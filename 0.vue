d93d1117de79772a27bd463827e86e44f1c2b589
yxDeveloper <dilutian0415@gmail.com>
2021-02-06 23:50:09 +0800

    define

--- 0.vue
+++ 0.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-01-17 23:58:40
- * @LastEditTime: 2021-02-06 23:49:06
+ * @LastEditTime: 2021-02-06 23:49:50
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue\0.vue
@@ -15,7 +15,13 @@
 
 <script>
     export default {
-        
+        define([
+            'require',
+            'dependency'
+        ], function(require, factory) {
+            'use strict';
+            
+        });
     }
 </script>
 
