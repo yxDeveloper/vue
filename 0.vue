dcdcd6bd40e715d44083c77596e1ae730d079012
yxDeveloper <dilutian0415@gmail.com>
2021-02-06 23:49:16 +0800

    textalign

--- 0.vue
+++ 0.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-01-17 23:58:40
- * @LastEditTime: 2021-02-06 23:47:41
+ * @LastEditTime: 2021-02-06 23:49:06
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue\0.vue
@@ -23,5 +23,6 @@
 div {
     display: block;
     overflow: hidden;
+    text-align: center;
 }
 </style>
