32547c6ef88746e9e0f5d6d05f067573e17ce8d5
yxDeveloper <dilutian0415@gmail.com>
2021-02-08 22:53:23 +0800

    updated

--- jiaban.vue
+++ jiaban.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-01-13 23:41:54
- * @LastEditTime: 2021-02-08 22:52:26
+ * @LastEditTime: 2021-02-08 22:53:10
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue\jiaban.vue
@@ -16,6 +16,9 @@
 <script>
     export default {
         // public static void main string ages
+        updated () {
+            ;
+        },
     }
 </script>
 
