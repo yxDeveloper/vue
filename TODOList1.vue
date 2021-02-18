9aebb902ae84e50378f6377a5b3cfbb5b09592a6
yxDeveloper <dilutian0415@gmail.com>
2021-02-18 22:53:43 +0800

    span

--- TODOList1.vue
+++ TODOList1.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-01-09 12:34:37
- * @LastEditTime: 2021-02-18 22:52:56
+ * @LastEditTime: 2021-02-18 22:53:25
  * @LastEditors: Please set LastEditors
  * @Description:  theSpiritOfResistanceToDisease
  * @FilePath: \vue\TODOList1.vue
@@ -14,6 +14,7 @@
         <span v-text="theSpiritOfResistanceToDiseaseHead"></span>
         <epidemicPreventionAndControl></epidemicPreventionAndControl>
         <div></div>
+        <p><span></span></p>
     </div>
   </div>
 </template>
