1ff5347058ddd52fbda5fcac557146d18d8ea21a
yxDeveloper <dilutian0415@gmail.com>
2021-02-24 23:54:32 +0800

    debug

--- 1.vue
+++ 1.vue
@@ -1,12 +1,11 @@
 <!--
  * @Author: your name
  * @Date: 2021-02-22 00:00:32
- * @LastEditTime: 2021-02-24 23:52:39
+ * @LastEditTime: 2021-02-24 23:54:27
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue\1.vue
 -->
-vue
 <template>
     <div>
 
