202b75dd2cc5ae608023d11bd5c570632829a87c
yxDeveloper <dilutian0415@gmail.com>
2021-02-28 00:19:12 +0800

    vue3

---
+++ alala.vue
@@ -0,0 +1,33 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-02-28 00:09:36
+ * @LastEditTime: 2021-02-28 00:09:45
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue\alala.vue
+-->
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import { reactive, toRefs } from '@vue/composition-api'
+
+export default {
+    setup () {
+        const state = reactive({
+            count: 0,
+        })
+    
+        return {
+            ...toRefs(state),
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
