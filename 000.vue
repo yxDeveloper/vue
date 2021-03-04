7df2246b68f914551e7311cf09e0c0f7a5219905
yxDeveloper <dilutian0415@gmail.com>
2021-03-04 23:08:08 +0800

    forin

--- 000.vue
+++ 000.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-03-02 20:15:25
- * @LastEditTime: 2021-03-02 20:17:20
+ * @LastEditTime: 2021-03-04 23:07:47
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue\000.vue
@@ -14,7 +14,12 @@
 
 <script>
     export default {
-        
+        for (const labrary in object) {
+            if (Object.hasOwnProperty.call(object, labrary)) {
+                const element = object[labrary];
+                
+            }
+        }
     }
 </script>
 
