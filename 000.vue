d8145caa223ccdd6cc5ef442075a7752bbaca35f
yxDeveloper <dilutian0415@gmail.com>
2021-03-07 22:42:58 +0800

    1

--- 000.vue
+++ 000.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-03-02 20:15:25
- * @LastEditTime: 2021-03-07 22:41:41
+ * @LastEditTime: 2021-03-07 22:42:47
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue\000.vue
@@ -14,13 +14,13 @@
 
 <script>
     export default {
-        for (const labrary in object) {
-            if (Object.hasOwnProperty.call(object, labrary)) {
-                const element = object[labrary];
-                laburary
-            }
-        }
-    }
+    //     for (const labrary in object) {
+    //         if (Object.hasOwnProperty.call(object, labrary)) {
+    //             const element = object[labrary];
+    //             laburary
+    //         }
+    //     }
+    // }
 </script>
 
 <style lang="scss" scoped>
