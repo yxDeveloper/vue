60fd09180de325bb86362a0548493ef56edf9f00
yxDeveloper <dilutian0415@gmail.com>
2021-03-13 21:50:51 +0800

    debug

--- last.vue
+++ last.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-03-13 21:47:31
- * @LastEditTime: 2021-03-13 21:49:24
+ * @LastEditTime: 2021-03-13 21:50:45
  * @LastEditors: Please set LastEditors
  * @Description: last
  * @FilePath: \vue\last.vue
@@ -14,12 +14,12 @@
 </template>
 
 <script lang="ts">
-    import { Component, Vue } from 'vue-property-decorator';
+    // import { Component, Vue } from 'vue-property-decorator';
 
-    @Component
-    export default class last extends Vue {
+    // @Component
+    // export default class last extends Vue {
         
-    }
+    // }
 </script>
 
 <style scoped>
