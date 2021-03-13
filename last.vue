ae8adac1c680bfaa032be8f039ed3dd2b69c43aa
yxDeveloper <dilutian0415@gmail.com>
2021-03-13 21:49:44 +0800

    class typeScript

--- last.vue
+++ last.vue
@@ -1,9 +1,27 @@
 <!--
  * @Author: your name
  * @Date: 2021-03-13 21:47:31
- * @LastEditTime: 2021-03-13 21:47:44
+ * @LastEditTime: 2021-03-13 21:49:24
  * @LastEditors: Please set LastEditors
  * @Description: last
  * @FilePath: \vue\last.vue
 -->
 
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+
+    @Component
+    export default class last extends Vue {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
