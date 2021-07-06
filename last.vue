ec5c4f5b4411f600065dfad32bfe28f4ef312b8c
yxDeveloper <dilutian0415@gmail.com>
2021-07-07 00:25:01 +0800

    text-align

--- last.vue
+++ last.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-03-13 21:47:31
- * @LastEditTime: 2021-07-07 00:23:56
+ * @LastEditTime: 2021-07-07 00:24:47
  * @LastEditors: Please set LastEditors
  * @Description: last
  * @FilePath: \vue\last.vue
@@ -48,5 +48,6 @@
             }
         }
         float: left;
+        text-align: left;
     }
 </style>
